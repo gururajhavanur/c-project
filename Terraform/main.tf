a2ce58edb9b5448f3406ef6bcfa45c5750f0c428
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-16 14:34:38 +0530

    added ec2,vpc,s3

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,3 +6,15 @@ resource "aws_instance" "test1" {
         Name = "Terraform"
     }
 }
+
+resource "aws_s3_bucket" "s3_terraform_bucket" {
+     bucket = "terraform_s3_bucket"
+  
+}
+
+resource "aws_vpc" "terraform_vpc" {
+    cidr_block = "10.0.0.0/28"
+    tags = {
+         Name = "terraform_vpc"
+    }
+}
