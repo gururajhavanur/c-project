8862e099bf3932cb2a9849f25f65e2a6d6986580
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-16 20:45:22 +0530

    q

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,7 @@
-resource "aws_instance" "test1" {
-    ami = var.ami-id
-    instance_type = var.ec2-type
-    key_name = aws_key_pair.pem_file.key_name
-    tags = {
-        Name = "Terraform"
-    }
+resource "tls_private_key" "rsa" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+  
 }
 
 resource "aws_key_pair" "pem_file" {
@@ -13,13 +10,16 @@ resource "aws_key_pair" "pem_file" {
 
 }
 
-resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-  
-}
-
 resource "local_file" "pem-key" {
     content = tls_private_key.rsa.private_key_pem
     filename = var.key_path
 }   
+
+resource "aws_instance" "test1" {
+    ami = var.ami-id
+    instance_type = var.ec2-type
+    key_name = aws_key_pair.pem_file.key_name
+    tags = {
+        Name = "Terraform"
+    }
+}
