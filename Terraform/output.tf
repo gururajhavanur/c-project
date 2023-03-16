122b7c923dd3656414a0e61f5f610b4c73f1a4e2
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-16 18:16:06 +0530

    added output variable

---
+++ Terraform/output.tf
@@ -0,0 +1,14 @@
+output "ec2_public_ip_address" {
+    value = aws_instance.test1.public_ip
+
+}
+
+output "ec2_private_ip_address" {
+    value = aws_instance.test1.private_ip
+
+}
+
+output "ec2_pem" {
+    value = aws_instance.test1.key_name
+
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "ec2-type" {
     type = string
+    default = "t2.micro"
 
 }
 
 variable "ami-id" {
     type = string
+    default = "ami-0f8ca728008ff5af4"
     
 }
