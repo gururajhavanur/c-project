d7cf7d2736dbb35b3fd6b9309c56f1ac19adf1cf
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-17 15:06:38 +0530

    changed some values

--- Terraform/main1.tf
+++ Terraform/main1.tf
@@ -1,21 +1,8 @@
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-0f8ca728008ff5af4"
-    instance_type = "t2.micro"
+    ami = var.ami_id
+    instance_type = var.ec2_instance
     tags = {
       "name" = "Terraform"
     }
 }
 
-resource "aws_s3_bucket" "s3-bucket-Terraform" {
-    bucket = "s3-bucket-terraform-21"
-    
-  
-}
-
-resource "aws_vpc" "terraform_vpc" {
-    cidr_block = "10.0.0.0/28"
-    instance_tenancy = "default"
-    tags = {
-      "name" = "terraform-vpc"
-    }
-}
---
+++ Terraform/variables1.tf
@@ -0,0 +1,8 @@
+variable "ec2_instance" {
+    type = string
+
+}
+  
+variable "ami_id" {
+    type = string
+}
