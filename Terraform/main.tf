3e985ef11bfd0f34b682065006953979733403c4
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-17 18:13:06 +0530

    added modules

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,70 +1,19 @@
-resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-  
+module "create_sg" {
+  source = "./modules/create_sg"
+  sg_name = var.sg_name 
 }
 
-resource "aws_key_pair" "pem_file" {
-    key_name = var.key_name
-    public_key = tls_private_key.rsa.public_key_openssh
+module "create_pem" {
+  source = "./modules/create_pem"
+  key_name = var.key_name
+  key_path = var.key_path
 
 }
 
-resource "local_file" "pem-key" {
-    content = tls_private_key.rsa.private_key_pem
-    filename = var.key_path
-}   
-
-
-
-resource "aws_security_group" "allow_ssh" {
-  name        = var.sg_name
-  description = "Allow SSH inbound traffic"
-
-
-  ingress {
-    
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
- 
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-resource "aws_instance" "test1" {
-    ami = var.ami-id
-    instance_type = var.ec2-type
-    key_name = aws_key_pair.pem_file.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    tags = {
-        Name = "Terraform"
-    }
-}
+module "create_ec2" {
+  source = "./modules/create_ec2"
+    ami-id = var.ec2_ami
+    ec2-type =var.ec2_type
+    ec2-pem = module.create_pem.ec2_pem
+    ec2-sg_id =module.create_sg.sg_id
+} 
