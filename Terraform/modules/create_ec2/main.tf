91a67e6fdbee2c57f7d768d597af6f2952c29390
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-21 01:15:20 +0530

    changed

--- Terraform/modules/create_ec2/main.tf
+++ Terraform/modules/create_ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test1" {
-    ami = var.ami-id
-    instance_type = var.ec2-type
+    ami= var.ec2_ami
+    instance_type = var.ec2_type
     key_name = var.ec2-pem
     vpc_security_group_ids = [var.ec2_sg_id]
     tags = {
