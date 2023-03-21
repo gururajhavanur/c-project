9c062d151957acf9e6b853ad0a0777ed36fe503c
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-21 14:11:13 +0530

    changed

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,20 +1,18 @@
 module "create_sg" {
-  source = "./modules/create_sg"
-  sg_name = var.root_sg_name 
+    source = "./modules/create_sg"
+    sg_name = var.root_sg_name
 }
 
 module "create_pem" {
-  source = "./modules/create_pem"
-  key_name = var.root_key_name
-  key_path = var.root_key_path
-
+    source = "./modules/create_pem"
+    key_name = var.root_key_name
+    key_path = var.root_key_path
 }
 
-module "create_ec2" {
-   source = "github.com/gururajhavanur/terra_modules.git//create_ec2"
-    ami-id = var.root.ec2_ami
-    ec2-type =var.root.ec2_type
-    ec2-pem = module.create_pem.ec2_pem
-    ec2_sg_id =module.create_sg.sg_id
-  } 
+module "create_ec2_2" {
+    source = "github.com/jaintpharsha/terra_module.git//create_ec2"
+    ec2_type = var.root_ec2_type
+    ec2_pem = module.create_pem.ec2_pem
+    ec2_sg_id = module.create_sg.sg_id
+}
 
