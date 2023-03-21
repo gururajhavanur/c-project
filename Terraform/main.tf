f653b2b32f0e01782cdaba94f198a55a20100c51
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-21 15:34:44 +0530

    added main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,7 +10,7 @@ module "create_pem" {
 }
 
 module "create_ec2_2" {
-    source = "github.com/jaintpharsha/terra_module.git//create_ec2"
+    source = "github.com/gururajhavanur/terra_modules.git//create_ec2"
     ec2_type = var.root_ec2_type
     ec2_pem = module.create_pem.ec2_pem
     ec2_sg_id = module.create_sg.sg_id
