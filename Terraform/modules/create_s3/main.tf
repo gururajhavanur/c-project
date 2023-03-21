8e86872b9b6f986b98df7195aab1c7072bd7fee8
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-21 13:59:42 +0530

    addd

--- Terraform/modules/create_s3/main.tf
+++ Terraform/modules/create_s3/main.tf
@@ -1,3 +1,8 @@
-resource "aws_s3_bucket" "aws-s3-bucket-terra"{
-    bucket = "aws-s3-bucket-terraform-21"
+resource "aws_s3_bucket" "s3_backend_terraform"{
+    bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_backend_sse" {
+    bucket = aws_s3_bucket.s3_backend_terraform.bucket
+}
+
