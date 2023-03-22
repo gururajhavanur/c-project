2c2edeec069c8a0a8a1e49653812fad0fd6d6f47
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-22 12:23:00 +0530

    changred

--- Terraform/modules/remote-exec/output.tf
+++
@@ -1,8 +0,0 @@
-output "jenkins_url" {
-    value = "http://${aws_instance.my_ec2_instance.public_ip}:8080"
-  
-}
-
-output "jenkins_admin_password" {
-  value = "${aws_instance.my_ec2_instance.private_ip}:8080/administratorPassword"
-}
