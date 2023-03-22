273c4623f9eb1d2e7e8ae57653141f4a7840948c
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-22 18:21:36 +0530

    added

--- Terraform/modules/remote-exec/main.tf
+++ Terraform/modules/remote-exec/main.tf
@@ -9,7 +9,7 @@ resource "null_resource" "file_copy" {
     
     provisioner "remote-exec" {
         inline = [
-          "sudo yum install -y jenkins java-11-openjdk-devel",
+         "sudo yum install -y jenkins java-11-openjdk-devel",
          "sudo yum -y install wget",
          "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
          "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
@@ -19,3 +19,21 @@ resource "null_resource" "file_copy" {
         ]
     }
 } 
+
+
+
+
+            # #!/bin/bash
+            # # Install Jenkins and Java
+            # sudo wget -O /etc/yum.repos.d/jenkins.repo \\
+            #     <https://pkg.jenkins.io/redhat-stable/jenkins.repo>
+            # sudo rpm --import <https://pkg.jenkins.io/redhat-stable/jenkins.io.key>
+            # sudo yum -y upgrade
+            # # Add required dependencies for the jenkins package
+            # sudo amazon-linux-extras install -y java-openjdk11
+            # sudo yum install -y jenkins
+            # sudo systemctl daemon-reload
+
+            # # Start Jenkins
+            # sudo systemctl enable jenkins
+            # sudo systemctl start jenkins
