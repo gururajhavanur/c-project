d53d3ecb827ad96c154846caf9656020c11dbaac
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-22 18:04:01 +0530

    added jenkins

--- Terraform/modules/remote-exec/main.tf
+++ Terraform/modules/remote-exec/main.tf
@@ -9,9 +9,13 @@ resource "null_resource" "file_copy" {
     
     provisioner "remote-exec" {
         inline = [
-          "sudo apt update -y",
-          "sudo apt install jq git -y",
-          "echo 'This is remote-exec example' > remote-exec.txt"
+          "sudo yum install -y jenkins java-11-openjdk-devel",
+         "sudo yum -y install wget",
+         "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+         "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+         "sudo yum upgrade -y",
+         "sudo yum install jenkins -y",
+         "sudo systemctl start jenkins",
         ]
     }
 } 
