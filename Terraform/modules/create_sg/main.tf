b41a6808b2107d680810c64429903baf5b6ce4dd
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-22 12:12:41 +0530

    added jenkins o/p

--- Terraform/modules/create_sg/main.tf
+++ Terraform/modules/create_sg/main.tf
@@ -35,6 +35,15 @@ resource "aws_security_group" "allow_ports" {
  
   }
 
+  ingress {
+    
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
   tags = {
     Name = "allow_tls"
   }
