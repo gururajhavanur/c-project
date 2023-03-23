a9cc57b52c2934babdd271b54502d8d1460d4d9e
Gururajhavanur <gururajhavanur2@gmail.com>
2023-03-23 08:07:20 +0530

    removed jenkins

--- Terraform/modules/remote-exec/main.tf
+++ Terraform/modules/remote-exec/main.tf
@@ -6,19 +6,22 @@ resource "null_resource" "file_copy" {
         private_key = file(var.ec2_pem)
         agent = false 
     }
+
+
     
-    provisioner "remote-exec" {
-        inline = [
-         "sudo yum install -y jenkins java-11-openjdk-devel",
-         "sudo yum -y install wget",
-         "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-         "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-         "sudo yum upgrade -y",
-         "sudo yum install jenkins -y",
-         "sudo systemctl start jenkins",
-        ]
-    }
-} 
+    
+#     provisioner "remote-exec" {
+#         inline = [
+#          "sudo yum install -y jenkins java-11-openjdk-devel",
+#          "sudo yum -y install wget",
+#          "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+#          "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+#          "sudo yum upgrade -y",
+#          "sudo yum install jenkins -y",
+#          "sudo systemctl start jenkins",
+#         ]
+#     }
+# } 
 
 
 
